7b53439c3bb1974d45d5f776cc09b55f58821416
celine <koelblc@bzz.ch>
2025-11-24 14:59:06 +0100

    created all tests

--- trafficlight.test.js
+++ trafficlight.test.js
@@ -1,5 +1,17 @@
-const traffic = require('./trafficlight');
+const { trafficLight } = require('./trafficlight');
 
-test("trafficlight returns green", () => {
-    expect(traffic('green')).toBe('Go');
-})
+test("returns WALK! for green", () => {
+    expect(trafficLight('green')).toBe('WALK!');
+});
+
+test("returns DON'T WALK! for red", () => {
+    expect(trafficLight('red')).toBe("DON'T WALK!");
+});
+
+test("returns ATTENTION! for orange", () => {
+    expect(trafficLight('orange')).toBe('ATTENTION!');
+});
+
+test("returns OUT OF ORDER for undefined", () => {
+    expect(trafficLight()).toBe('OUT OF ORDER');
+});
